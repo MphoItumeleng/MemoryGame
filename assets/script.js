e3c314de91ad9a37a277912e1d7c6598e0ec5e44
Mpho Itumeleng <mphoitumeleng021@gmail.com>
2024-04-23 23:24:23 +0200

    Made style changes

--- assets/script.js
+++ assets/script.js
@@ -36,12 +36,12 @@ let movesCount = 0,
 //For timer
 const timeGenerator = () => {
   seconds += 1;
-  //minutes logic
+
   if (seconds >= 60) {
     minutes += 1;
     seconds = 0;
   }
-  //format time before displaying
+  //Format time before displaying
   let secondsValue = seconds < 10 ? `0${seconds}` : seconds;
   let minutesValue = minutes < 10 ? `0${minutes}` : minutes;
   timeValue.innerHTML = `<span>Time:</span>${minutesValue}:${secondsValue}`;
@@ -55,34 +55,31 @@ const movesCounter = () => {
 
 //Pick random objects from the items array
 const generateRandom = (size = 4) => {
-  //temporary array
   let tempArray = [...items];
-  //initializes cardValues array
   let cardValues = [];
-  //size should be double (4*4 matrix)/2 since pairs of objects would exist
+  //Size should be double (4*4 matrix)/2 since pairs of objects would exist
   size = (size * size) / 2;
-  //Random object selection
+
   for (let i = 0; i < size; i++) {
     const randomIndex = Math.floor(Math.random() * tempArray.length);
     cardValues.push(tempArray[randomIndex]);
-    //once selected remove the object from temp array
     tempArray.splice(randomIndex, 1);
   }
+
   return cardValues;
 };
 
 const matrixGenerator = (cardValues, size = 4) => {
   gameContainer.innerHTML = "";
   cardValues = [...cardValues, ...cardValues];
-  //simple shuffle
   cardValues.sort(() => Math.random() - 0.5);
   for (let i = 0; i < size * size; i++) {
     /*
-        Create Cards
-        before => front side (contains question mark)
-        after => back side (contains actual image);
-        data-card-values is a custom attribute which stores the names of the cards to match later
-      */
+      Create Cards
+      before => front side (contains question mark)
+      after => back side (contains actual image);
+      data-card-values is a custom attribute which stores the names of the cards to match later
+    */
     gameContainer.innerHTML += `
      <div class="card-container" data-card-value="${cardValues[i].name}">
         <div class="card-before">?</div>
@@ -91,6 +88,7 @@ const matrixGenerator = (cardValues, size = 4) => {
      </div>
      `;
   }
+  
   //Grid
   gameContainer.style.gridTemplateColumns = `repeat(${size},auto)`;
 
